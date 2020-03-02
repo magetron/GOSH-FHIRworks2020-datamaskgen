165109690898c85e2869e288d0154ace85584b0a
Patrick Wu <magetron1@gmail.com>
2020-03-02 18:02:52 +0000

    introduce json parser

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
 int main(int argc, char** argv) {
     auto response = cpr::Get(cpr::Url{"https://httpbin.org/get"});
-    cout << response.text << endl;
+    auto json = nlohmann::json::parse(response.text);
+    cout << json.dump(4) << endl;
+    return 0;
 }
