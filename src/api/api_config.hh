c4cecf91759995a05ea7e433e9d36ba716a1f20c
Patrick Wu <magetron1@gmail.com>
2020-03-05 03:54:11 +0000

    fix minor issue

--- src/api/api_config.hh
+++ src/api/api_config.hh
@@ -11,6 +11,6 @@ using namespace std;
 
 static const string CACHE_FOLDER("/Users/patrick/Documents/University-College-London/UCL-CS/Year-2/COMP0016/FHIR-Hackathon/GOSH-FHIRworks2020-datamasker/cache/");
 
-static const string PATIENTS_JSON_API_ENDPOINT("\"https://localhost:5001/api/Patient\"")
+static const string PATIENTS_JSON_API_ENDPOINT("\"https://localhost:5001/api/Patient\"");
 
 #endif
